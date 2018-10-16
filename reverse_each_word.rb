5632a9eb47e7e44e8b3793c0f9096bbe3f6293a2
Jane Venter <janeanneventer@gmail.com>
2018-10-16 16:45:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   ar = string.split(' ')
-  ar.reverse
-  #ar.each do |word|
-    #word.reverse
-  end
+  ar.each do |word|
+    smallar = word.split('')
+    
+  #end
 end
